cb0e6dd759130e6a09558ff044b58bf682c1aef2
Hari-2020wb86121 <2020wb86121@wilp.bits-pilani.ac.in>
2024-06-04 17:49:32 +0530

    DevOpsExample.java

---
+++ DevOpsExample.java
@@ -0,0 +1,9 @@
+
+/**
+* The HelloWorldApp class implements an applicaiton that simply prints "Herllo World" to standard output.
+*/
+public class DevOpsExample {
+public static void main (String[] args){
+System.out.println("Welcome to DevOps");
+}
+}
